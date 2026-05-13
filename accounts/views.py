60c32d018bd0716da31d1bf3e47b497ff09ea189
GreyStil <renatrubatski2@gmail.com>
2026-05-14 02:38:16 +0300

    Добавлена возможность удалять игры из Желаемого

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,6 @@
-from shop.utils.achievements import check_and_award_achievements
+class RemoveFromFavoritesView(LoginRequiredMixin, View):
+    def post(self, request, game_id):
+        game = get_object_or_404(Game, id=game_id)
+        request.user.profile.favorites.remove(game)
+        messages.success(request, f'"{game.title}" удалена из желаемого')
+        return redirect('accounts:profile_favorites')
